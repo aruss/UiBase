11fff54e76cde700a8e1dbb9db6f006ab68793c4
aruss <arusslabs@gmail.com>
2017-08-02 22:43:31 +0200

    sidebar fixes

--- src/components/topbar/topbar.vue
+++ src/components/topbar/topbar.vue
@@ -4,20 +4,19 @@
     <div class="topbar-logo">
       <div class="text-center">
         <a href="/" class="logo">
-          <i class="icon-magnet icon-c-logo"></i>
-          <span>UiBASE</span>
+          <i class="icon dripicons-home"></i>
         </a>
       </div>
     </div>
 
     <div class="topbar-content">
 
-      <ul class="menu-left">
+      <!--<ul class="menu-left">
         <li>
            <button type="button" class="btn"
             v-on:click.stop="$broadcast('sidebartoggle')"><i class="dripicons-menu"></i></button>
         </li>
-      </ul>
+      </ul>-->
 
       <ul class="menu-right">
         <li>
@@ -27,9 +26,9 @@
               <img src="./avatar-1.jpg" alt="user" class="rounded-circle">
             </button>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#"><i class="ti-home"></i><span>Action</span></a>
-              <a class="dropdown-item" href="#"><i class="ti-home"></i><span>Another action</span></a>
-              <a class="dropdown-item" href="#"><i class="ti-home"></i><span>Something else here</span></a>
+              <a class="dropdown-item" href="#"><i class="dripicons-user"></i><span>Action</span></a>
+              <a class="dropdown-item" href="#"><i class="dripicons-inbox"></i><span>Another action</span></a>
+              <a class="dropdown-item" href="#"><i class="dripicons-gear"></i><span>Something else here</span></a>
             </div>
           </div>
         </li>
